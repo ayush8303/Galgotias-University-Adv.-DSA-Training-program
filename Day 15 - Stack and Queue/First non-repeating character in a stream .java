a5a66f7d26b0d747cd744472868d39a2455868e1
Ayush Tripathi <arnavtripathi1234@gmail.com>
2024-04-03 14:26:38 +0530

    update

--- Day 15 - Stack and Queue/First non-repeating character in a stream .java
+++ Day 15 - Stack and Queue/First non-repeating character in a stream .java
@@ -1,22 +1,32 @@
-class Solution {
-	public:
-		string FirstNonRepeating(string A){
-		    queue<char> q;
-		    int freq[26]={0};
-		    string ans="";
-		    for(char ch:A){
-		        freq[ch-'a']++;
-		        q.push(ch);
-		        while(!q.empty() && freq[q.front()-'a']>1){
-		            q.pop();
-		        }
-		        if(q.empty()){
-		            ans+='#';
-		        }
-		        else {
-		            ans+=q.front();
-		        }
-		    }
-		    return ans;
-		}
-};
+class Solution
+{
+    public String FirstNonRepeating(String A)
+    {
+        Queue<Character> q=new LinkedList<>();
+        int n=A.length();
+        int[] freq=new int[26];
+        StringBuilder ans=new StringBuilder();
+        
+        for(int i=0;i<n;i++){
+            char ch=A.charAt(i);
+            
+            freq[ch-'a']++;
+            
+            q.offer(ch); 
+            
+            while(!q.isEmpty() && freq[q.peek()-'a']>1){
+                q.poll(); 
+            }
+            
+            if(q.isEmpty()){
+                ans.append('#');
+            }
+            else {
+                ans.append(q.peek());
+            }
+            
+        }
+        return ans.toString();
+        
+    }
+}
